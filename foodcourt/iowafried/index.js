05b67a2f889fda374f6b4b34b080c796a86455b3
Bob Reselman <bob@CogArtTech.com>
2019-09-08 00:18:56 -0700

    fixed typo

--- foodcourt/iowafried/index.js
+++ foodcourt/iowafried/index.js
@@ -3,7 +3,7 @@ const port = process.env.APP_PORT || 3000;
 
 const restaurant = 'Iowa Fried Chicken';
 
-const foods = ['Chix Pack', '20 Piece Bucket', 'Chicken Pot Pir', 'Spicy Wings']
+const foods = ['Chix Pack', '20 Piece Bucket', 'Chicken Pot Pie', 'Spicy Wings']
 
 const sample = (items) => {return items[Math.floor(Math.random()*items.length)];};
 

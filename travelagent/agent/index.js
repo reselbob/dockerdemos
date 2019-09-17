1711fb2d72ce174e52450d5b4f7d25db34066021
reselbob <reselbob@gmail.com>
2019-09-16 19:29:29 -0700

    building out travel agent

---
+++ travelagent/agent/index.js
@@ -0,0 +1,66 @@
+const http = require('http');
+const port = process.env.APP_PORT || 3000;
+
+const agent = 'Reselbob Travel';
+
+const services = ['auto', 'lodging', 'airline'];
+
+const sample = (items) => {return items[Math.floor(Math.random()*items.length)];};
+
+const handleRequest = (request, response)  => {
+
+    const service = sample(services);
+
+    http.get(`http://${service}:3000`, (resp) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            const obj = JSON.parse(data);
+            obj.agent = agent;
+            const str = JSON.stringify(obj);
+            response.setHeader("Content-Type", "application/json");
+            response.writeHead(200);
+            response.end(str);
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+        const str = JSON.stringify(err);
+        response.setHeader("Content-Type", "application/json");
+        response.writeHead(500);
+        response.end(str);
+    });
+};
+
+const server = http.createServer(handleRequest);
+
+server.listen(port, () => {
+    console.log(`${agent} API Server is listening on port ${port}`);
+});
+
+
+const shutdown = (signal) => {
+    if(!signal){
+        console.log(`${agent} API Server shutting down at ${new Date()}`);
+    }else{
+        console.log(`Signal ${signal} : ${agent} API Server shutting down at ${new Date()}`);
+    }
+    server.close(function () {
+        process.exit(0);
+    })
+};
+process.on('SIGTERM', function () {
+    shutdown('SIGTERM');
+});
+
+process.on('SIGINT', function () {
+    shutdown('SIGINT');
+});
+
+module.exports = {server,shutdown};

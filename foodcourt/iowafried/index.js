d469688b1c7225aa92afbab1462386f1a56d3b8e
Bob Reselman <reselbob@gmail.com>
2020-09-21 14:42:13 -0700

    trying to get interprocess trace to work

--- foodcourt/iowafried/index.js
+++ foodcourt/iowafried/index.js
@@ -1,10 +1,10 @@
 const http = require('http');
 const port = process.env.APP_PORT || 3000;
-/*
+
 const initTracer = require('./tracer').initTracer;
 const { Tags, FORMAT_HTTP_HEADERS } = require('opentracing');
 const tracer = initTracer('foodcourt_iowafried');
-*/
+
 const restaurant = 'Iowa Fried Chicken';
 
 const foods = ['Chix Pack', '20 Piece Bucket', 'Chicken Pot Pie', 'Spicy Wings'];
@@ -23,31 +23,31 @@ const shutdown = (signal) => {
 };
 
 const server = http.createServer((request, response) => {
-    /*
+
     parentSpanContext = tracer.extract(FORMAT_HTTP_HEADERS, request.headers)
     const span = tracer.startSpan('iowafried_service_request', {
         childOf: parentSpanContext,
         tags: { [Tags.SPAN_KIND]: Tags.SPAN_KIND_RPC_SERVER }
     });
-*/
+
     const order = sample(foods) ;
-/*
+
     span.setTag('indentified_order', order);
     span.log({
         'event': 'iowafried_service_request',
         'value': order
     });
-*/
+
     const str = JSON.stringify({restaurant, order});
 
-    //span.setTag(Tags.HTTP_STATUS_CODE, 200)
-    //span.setTag('iowafried_call_result', str)
+    span.setTag(Tags.HTTP_STATUS_CODE, 200)
+    span.setTag('iowafried_call_result', str)
 
     response.setHeader("Content-Type", "application/json");
     response.writeHead(200);
     response.end(str);
 
-    //span.finish();
+    span.finish();
 }).listen(port, (err) => {
     console.log(`${restaurant} API Server is started on ${port}  at ${new Date()} with pid ${process.pid}`);
 });

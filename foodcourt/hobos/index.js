4ef34fc5ef02140a1e4d55219aa4d6024815ecea
Bob Reselman <reselbob@gmail.com>
2020-09-21 16:21:49 -0700

    working iowafried bug

--- foodcourt/hobos/index.js
+++ foodcourt/hobos/index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const port = process.env.APP_PORT || 3000;
 
-const initTracer = require('./tracing').initTracer;
+const initTracer = require('./tracer').initTracer;
 const { Tags, FORMAT_HTTP_HEADERS } = require('opentracing');
 const tracer = initTracer('hobos');
 
